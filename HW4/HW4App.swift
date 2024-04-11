40ffc4adbc8a5c687c8dfaff242c3e436ef05060
Ashwamegha Holkar <53247119+ashunitinholkar@users.noreply.github.com>
2024-04-11 12:34:11 -0700

    Initial Commit

---
+++ HW4/HW4App.swift
@@ -0,0 +1,17 @@
+//
+//  HW4App.swift
+//  HW4
+//
+//  Created by Ashwamegha Holkar on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HW4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
